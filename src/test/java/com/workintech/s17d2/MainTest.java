0b11fa27af866d0320d3a2d22045278a88d29483
mertsaydan <mertsaydan85@gmail.com>
2026-07-21 21:45:17 +0300

    Mert Saydan

--- src/test/java/com/workintech/s17d2/MainTest.java
+++ src/test/java/com/workintech/s17d2/MainTest.java
@@ -1,7 +1,6 @@
 package com.workintech.s17d2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.workintech.s17d2.model.*;
 import com.workintech.s17d2.rest.DeveloperController;
 import com.workintech.s17d2.tax.DeveloperTax;
 import org.junit.jupiter.api.*;
